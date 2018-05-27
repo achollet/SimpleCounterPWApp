1aeb636f472b38726fc08922f1e98d8fa6e72bbe
A. Chollet <alpchollet+github@gmail.com>
2018-05-27 08:57:07 +0200

    Cleaning repo of unused files

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="container">
-    <div class="serie-label label">
-      SERIES
-    </div>
-    <div class="repetition-label label">
-      REPETITIONS
-    </div>
-
-    <div class="serie-value label">
-      X {{series}}
-    </div>
-    <div class="value-separator label">/</div>
-    <div class="repetition-value label">
-      {{repetitions}}
+    <div class="label-container label">
+      <!-- Labels Row -->
+      <div class="serie-label"> SERIES </div>
+      <div class="repetition-label"> REPETITIONS </div>
+      <!-- Values Display-->
+      <div class="serie-value">{{series}}</div>
+      <div class="value-separator">/</div>
+      <div class="repetition-value">{{repetitions}}</div>
     </div>
 
-    <round-button icon='add' :onClick=addOneSerie class="add-button"></round-button>
-    <round-button icon='repeat' :onClick=addOneSerie class="repeat-button"></round-button>
-    <round-button icon='clear' :onClick=reset class="reset-button"></round-button>
+    <div class="buttons-container">
+      <round-button icon='add' :onClick=addOneRep class="add-button label"></round-button>
+      <round-button icon='repeat' :onClick=addOneSerie class="repeat-button label"></round-button>
+      <round-button icon='clear' :onClick=reset class="reset-button label"></round-button>
+    </div>
   </div>
 </template>
 
@@ -50,19 +47,33 @@ export default {
 
 <style>
 .container {
-  width: 100%;
   height: 100%;
   display: grid;
-  grid-template-columns: 10% 37.5% 5% 37.5% 10%;
-  grid-template-rows: 20px repeat(2, 40px) 40px 60px 10% 20px;
-  background-color: cornflowerblue;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 2fr;
+  grid-template-areas: 'label-container' 'buttons-container';
+  background-color:#9c43bc;
+}
+
+.label-container {
+  display: grid;
+  grid-template-columns: 20px 1fr 10% 1fr 20px;
+  grid-template-rows: 20px 1fr 40px 1fr 20px;
+  grid-row-gap: 10px;
+}
+
+.buttons-container {
+  display: grid;
+  grid-template-columns: 20px 1fr 10% 1fr 20px;
+  grid-template-rows: 20px 1fr 40px 1fr 20px;
+  grid-row-gap: 10px;
 }
 
 .label {
   color: white;
   font-family: monospace;
-  font-size: 2.5em;
-  font-weight: 900;
+  font-size: 1.7em;
+  font-weight: 600;
 }
 
 .serie-label {
@@ -111,8 +122,8 @@ export default {
 }
 
 .add-button {
-  grid-row-start: 5;
-  grid-row-end: 6;
+  grid-row-start: 2;
+  grid-row-end: 3;
   grid-column-start: 2;
   grid-column-end: 5;
   justify-self: center;
@@ -120,8 +131,8 @@ export default {
 }
 
 .repeat-button {
-  grid-row-start: 6;
-  grid-row-end: 7;
+  grid-row-start: 4;
+  grid-row-end: 5;
   grid-column-start: 2;
   grid-column-end: 3;
   justify-self: center;
@@ -129,8 +140,8 @@ export default {
 }
 
 .reset-button {
-  grid-row-start: 6;
-  grid-row-end: 7;
+  grid-row-start: 4;
+  grid-row-end: 5;
   grid-column-start: 4;
   grid-column-end: 5;
   justify-self: center;
--- src/components/RoundButton.vue
+++ src/components/RoundButton.vue
@@ -1,18 +1,28 @@
 <template>
       <button v-on:click=onClick class="mdl-button mdl-js-button rounded-button">
-        <i class="material-icons">{{ icon }}</i>
+        <i class="material-icons .round-button-icon">{{ icon }}</i>
       </button>
 </template>
 
 <style scoped>
 .rounded-button
 {
-  border-color: white;
-  border-width: 2px;
+  width: 10%;
+  height: 10%;
+  padding-bottom: 10%;
+  border-radius: 50%;
+  border: 3px solid white;
+  overflow: hidden;
+}
+
+.round-button-icon {
+    display: block;
+    width: 76%;
+    padding: 12%;
+    height: auto;
 }
 </style>
 
-<
 <script>
   export default {
     name: 'round-button',
